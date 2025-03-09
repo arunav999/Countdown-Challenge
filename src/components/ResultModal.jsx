732c01065321633effd966dba4fc50cbcdad1155
Arunav Singh <arunavsingh999@gmail.com>
2025-03-09 22:31:56 +0530

    sharing state across components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,10 +1,13 @@
 import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { result, targetTime },
+  { targetTime, remainingTime, onReset },
   ref
 ) {
   const dialog = useRef();
+
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
   useImperativeHandle(ref, () => {
     return {
@@ -17,7 +20,7 @@ const ResultModal = forwardRef(function ResultModal(
   return (
     <>
       <dialog ref={dialog} className="result-modal">
-        <h2>You {result}</h2>
+        {userLost && <h2>You lost</h2>}
         <p>
           The target time was{" "}
           <strong>
@@ -25,9 +28,13 @@ const ResultModal = forwardRef(function ResultModal(
           </strong>
         </p>
         <p>
-          You stopped the timer with <strong>X seconds left.</strong>
+          You stopped the timer with{" "}
+          <strong>
+            {formattedRemainingTime} second
+            {formattedRemainingTime > 1 ? "s" : ""} left.
+          </strong>
         </p>
-        <form method="dialog">
+        <form method="dialog" onSubmit={onReset}>
           <button>Close</button>
         </form>
       </dialog>
