3f4bb252345d5a80bb577bf636da22ed0f5a7c75
Arunav Singh <arunavsingh999@gmail.com>
2025-03-09 12:04:08 +0530

    managing state with two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [state, setState] = useState({
+    playerName: "",
+    submitted: false,
+  });
+
+  function handleNameChange(event) {
+    setState({
+      submitted: false,
+      playerName: event.target.value,
+    });
+  }
+
+  function handleClick() {
+    setState((prev) => ({
+      ...prev,
+      submitted: true,
+    }));
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {state.submitted ? state.playerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleNameChange} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
