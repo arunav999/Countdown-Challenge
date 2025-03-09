5104463643e439dd8bcd35eeb53dfb0a4fb8d2b3
Arunav Singh <arunavsingh999@gmail.com>
2025-03-09 19:07:23 +0530

    exposing components api via the useImperativeHandle Hook

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ ref, result, targetTime }) {
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
     <>
-      <dialog ref={ref} className="result-modal">
+      <dialog ref={dialog} className="result-modal">
         <h2>You {result}</h2>
         <p>
           The target time was{" "}
@@ -20,4 +33,6 @@ export default function ResultModal({ ref, result, targetTime }) {
       </dialog>
     </>
   );
-}
+});
+
+export default ResultModal;
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -16,7 +16,7 @@ export default function TimerChallenge({ title, targetTime }) {
       setState({
         timerExpired: true,
       });
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
 
     setState({
